a74963539a38c05955aa67dccf3d9447ebe1bcc1
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-10-18 02:31:30 +0200

    Project structure and add some images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo : 'products'
+    },
+    { path: 'products', loadComponent: () => import('./pages/products/products').then(m => m.Products) },
+    { path: 'wishlist', loadComponent: () => import('./pages/wish-list/wish-list').then(m => m.WishList) },
+
+];
